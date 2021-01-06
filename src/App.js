699b298302a14ce7bdb8f9af8da8181f4b15f151
lee-hyunsu <dlgustn675@naver.com>
2021-01-06 12:18:41 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import PostListPage from './pages/PostListPage';
 import RegisterPage from './pages/RegisterPage';
 
 function App(){
   return(
     <>
+      <Route component={PostListPage} path=""/>
       <Route component={LoginPage} path="/login"/>
       <Route component={RegisterPage} path="/register"/>
     </>
---
+++ src/pages/PostListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/common/Header';
+
+const PostListPage = () => {
+    return (
+        <>
+            <Header />
+            <div>무슨 API를 사용할래?</div>
+        </>
+    );
+};
+
+export default PostListPage;
