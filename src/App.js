3a7226a6d181f90e51ae15fd446808356c20dcff
lee-hyunsu <dlgustn675@naver.com>
2021-01-20 00:03:01 +0900

    test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Route} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
 
 function App(){
   return(
     <>
       <Route component={MainPage} path=""/>
+      <Route component={WritePage} path="/write"/>
       <Route component={LoginPage} path="/login"/>
       <Route component={RegisterPage} path="/register"/>
     </>
