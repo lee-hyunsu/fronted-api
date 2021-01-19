c3c07b08957ceff69f1248b2d23cddc884f18a05
lee-hyunsu <dlgustn675@naver.com>
2021-01-19 22:51:43 +0900

    test

---
+++ src/components/write/Editor.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useRef} from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+
+const EditorBlock = styled(Responsive)`
+    padding-top: 5rem;
+    padding-bottom: 5rem;
+`
