386162cd4447a860f32c5de9c76c85c7e92c5919
lee-hyunsu <dlgustn675@naver.com>
2021-01-19 23:47:45 +0900

    test

--- src/components/write/Editor.js
+++ src/components/write/Editor.js
@@ -8,4 +8,35 @@ import Responsive from '../common/Responsive';
 const EditorBlock = styled(Responsive)`
     padding-top: 5rem;
     padding-bottom: 5rem;
-`
+`;
+
+const TitledInput = styled.input`
+    font-size: 3rem;
+    outline: none;
+    padding-bottom: 0.5rem;
+    border: none;
+    border-bottom: 1px solid ${palette.gray[4]};
+    margin-bottom: 2rem;
+    width: 100%;
+`;
+
+const QuillWrapper = styled.div`
+    .ql-editor {
+        padding: 0;
+        min-height: 320px;
+        font-size: 1.125rem;
+        line-height: 1.5;
+    }
+    .ql-editor.ql-blank::before {
+        left: 0px;
+    }
+`;
+
+const Editor = () => {
+    const quillElement = useRef(null); //Quill을 적용할 DivElement를 설정
+    const quillnstance = useRef(null); //Quill인스턴스를 설정
+
+    useEffect(() => {
+        
+    })
+}
