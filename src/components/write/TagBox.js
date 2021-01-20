8ae35f8be959561fa0d4998ff25eedf153692c05
lee-hyunsu <dlgustn675@naver.com>
2021-01-20 16:28:29 +0900

    test

--- src/components/write/TagBox.js
+++ src/components/write/TagBox.js
@@ -10,6 +10,55 @@ const TagBoxBlock = styled.div`
     h4{
         color: ${palette.gray[8]};
         margin-top: 0;
-
+        margin-bottom: 0.5rem;
     }
 `;
+
+const TagForm = styled.form`
+    border-radius: 4px;
+    overflow: hidden;
+    display: flex;
+    width: 256px;
+    border: 1px solid ${palette.gray[9]};
+    input,
+    button {
+        outline: none;
+        border: none;
+        font-size: 1rem;
+    }
+
+    input {
+        padding: 0.5rem;
+        flex: 1;
+    }
+
+    button {
+        cursor: pointer;
+        padding-right: 1rem;
+        padding-left: 1rem;
+        border: none;
+        background: ${palette.gray[8]};
+        color: white;
+        font-weight: bold;
+        &:hover {
+            background: ${palette.gray[6]};
+        }
+    }
+`;
+
+const Tag = styled.div`
+    margin-right: 0.5rem;
+    color: ${palette.gray[6]};
+    cursor: pointer;
+    &:hover{
+        opacity: 0.5;
+    }
+`;
+
+const TagListBlock = styled.div`
+    display: flex;
+    margin-top: 0.5rem;
+`;
+
+//React.memo를 사용하여 tag값이 바뀔 때만 리렌더링 되도록 처리
+const TagItem = React.memo(({tag}) => <Tag>#{tag}</Tag>)
