10b1afea6c3a000deeb6ab28cc7db040c6f24a73
lee-hyunsu <dlgustn675@naver.com>
2021-01-22 21:22:08 +0900

    test

--- src/modules/write.js
+++ src/modules/write.js
@@ -1,4 +1,4 @@
-import {createAction, handleAction} from 'redux-actions';
+import {createAction, handleActions} from 'redux-actions';
 
 const INITIALIZE = 'write/INITIALIZE'; //모든 내용 초기화,
 const CHANGE_FIELD = 'write/CHANGE_FIELD'; //특정 key 값 바꾸기
@@ -8,3 +8,22 @@ export const changeField = createAction(CHANGE_FIELD, ({key, value}) => ({
     key,
     value,
 }));
+
+const initialState = {
+    title: '',
+    body: '',
+    tags: [],
+};
+
+const write = handleActions(
+    {
+        [INITIALIZE]: state => initialState, //initialState를 넣으면 초기 상태로 바꾸게끔
+        [CHANGE_FIELD]: (state, {payload: {key, value}}) => ({
+            ...state,
+            [key]: value, //특정 key값을 업데이트
+        }),
+    },
+    initialState,
+);
+
+export default write;
