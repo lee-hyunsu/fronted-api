e3d95163682e7aedf7ca0c789671230e422862a3
lee-hyunsu <dlgustn675@naver.com>
2021-01-23 17:09:18 +0900

    test

--- src/container/write/EditorContainer.js
+++ src/container/write/EditorContainer.js
@@ -1,14 +1,27 @@
 import React, {useEffect, useCallback} from 'react';
 import Editor from '../../components/write/Editor';
 import {initialize} from '../../modules/write';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import { changeField } from '../../modules/auth';
+
 
 const EditorContainer = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const {title, body} = useSelector(({write}) => ({
+        title: write.title,
+        body: write.body,
+    }));
 
-    return () => {
-        dispatch(initialize());
-    };
-}
+    const onChangeField = useCallback(payload => 
+        dispatch(changeField(payload)), [dispatch,]
+    );
+
+    useEffect(() => {
+        return () => {
+            dispatch(initialize());
+        };
+    }, [dispatch]);
+    return <Editor onChangeField={onChangeField} title={title} body={body} />
+    }
 
 export default EditorContainer;
