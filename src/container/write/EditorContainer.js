25b6628bf612ae0a45ac7bd033dc1112ba2f04b4
lee-hyunsu <dlgustn675@naver.com>
2021-01-23 16:51:58 +0900

    test

--- src/container/write/EditorContainer.js
+++ src/container/write/EditorContainer.js
@@ -1,10 +1,14 @@
 import React, {useEffect, useCallback} from 'react';
 import Editor from '../../components/write/Editor';
+import {initialize} from '../../modules/write';
+import {useDispatch} from 'react-redux';
 
 const EditorContainer = () => {
-    const dispatch = dispatch()
+    const dispatch = useDispatch()
 
     return () => {
         dispatch(initialize());
     };
 }
+
+export default EditorContainer;
